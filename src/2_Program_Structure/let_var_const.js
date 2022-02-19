ea2b5319ead74fbe54f23f9b8d994b96629b2fe3
Daniel Hernandez de Leon <alu0101331720@ull.edu.es>
2022-02-19 19:58:19 +0000

    disable_var

--- src/2_Program_Structure/let_var_const.js
+++ src/2_Program_Structure/let_var_const.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const a = 5;
+// eslint-disable-next-line no-var
 var b = 10;
 
 if (a === 5) {
   const a = 4;
+  // eslint-disable-next-line no-var
   var b = 1;
 
   console.log(a);
