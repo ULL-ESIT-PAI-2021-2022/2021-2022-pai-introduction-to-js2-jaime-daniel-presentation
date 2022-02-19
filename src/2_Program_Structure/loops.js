4eb7c570126b502139b8cdb283f82319d55abd25
Jaime <alu0101329093@ull.edu.es>
2022-02-19 20:09:17 +0000

    Moved loops to separete folders

--- src/2_Program_Structure/loops.js
+++ src/2_Program_Structure/loops.js
@@ -15,14 +15,3 @@ do {
 for (let i = 0; i < 10; i++) {
   console.log(i);
 }
-
-const obj = {a: 'Hello', b: 'World', c: '!'};
-// eslint-disable-next-line guard-for-in
-for (const element in obj) {
-  console.log(element, obj[element]);
-}
-
-const list = ['Hello', 'World', '!'];
-for (const element of list) {
-  console.log(element);
-}
---
+++ src/4_Objects_Arrays/Arays/array_loop.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const list = ['Hello', 'World', '!'];
+for (const element of list) {
+  console.log(element);
+}
