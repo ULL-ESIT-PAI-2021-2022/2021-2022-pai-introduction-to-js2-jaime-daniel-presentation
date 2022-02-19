a2965ce598b92bd52bbb3acb53045038560fbc7f
Daniel Hernandez de Leon <alu0101331720@ull.edu.es>
2022-02-19 11:58:48 +0000

    Added_Arrays

--- src/4_Objects_Arrays/Arays/arrays.js
+++ src/4_Objects_Arrays/Arays/arrays.js
@@ -1,3 +1,19 @@
 'use strict';
 
+function main() {
+  let array = [1, 2, 3, 4, 5];
+  console.log(array.length);
 
+  let string = "Hello";
+  console.log(string.length);
+  console.log(string.toUpperCase());
+
+  for (let i = array.length; i < 100; i++) {
+    array.push(i);
+  }
+  console.log(array);
+}
+
+if (require.main === module) {
+  main();
+}
