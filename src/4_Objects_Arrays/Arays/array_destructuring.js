ebc0c48852891478785e36473456012db6b73316
Jaime <alu0101329093@ull.edu.es>
2022-02-19 17:47:05 +0000

    Example for array destructuring

---
+++ src/4_Objects_Arrays/Arays/array_destructuring.js
@@ -0,0 +1,9 @@
+const array = ['a', {b: 'b'}, ['c'], 1, true];
+
+const [a, b, c, d, e] = array;
+console.log(a, b, c, d, e);
+// a { b: 'b' } [ 'c' ] 1 true
+
+const [f, g] = array;
+console.log(f, g);
+// a { b: 'b' }
